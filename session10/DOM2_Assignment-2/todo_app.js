94eb659fa8943fb8647719d31e2ee043ce1f8e0b
nihal <nihalmmuneer@gmail.com>
2022-02-24 12:28:33 +0530

    updated

--- session10/DOM2_Assignment-2/todo_app.js
+++ session10/DOM2_Assignment-2/todo_app.js
@@ -4,7 +4,7 @@ function add() {
   let inputValue = document.getElementById("myInput").value;
   let t = document.createTextNode(inputValue);
   console.log(t);
-  li.appendChild(t);
+  li.appendChild(t); 
   console.log(li)
   if (inputValue === '') {
     alert("You must write something!");
@@ -13,18 +13,54 @@ function add() {
   }
   document.getElementById("myInput").value = "";
 
+  let check = document.createElement("INPUT");
+  check.type = 'checkbox';
+  check.className = "checkBox";
+ 
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
+  li.append(check)
  
   let close = document.getElementsByClassName("close");
   for ( let i = 0; i < close.length; i++) {
     close[i].onclick = function() {
+      
       let div = this.parentElement;
       console.log(div);
       div.style.display = "none";
+
     }
   }
-}
+
+  // mark and unmark the checkbox 
+  let active = document.getElementsByClassName("checkBox");
+  console.log(active);
+  
+  let i;
+      for(i = 0; i < active.length ; i++)
+      active[i].addEventListener('click',function(){
+      if(this.checked)
+      {
+        //on checked the text inside the parent element have a line-through 
+        console.log('checked');
+        let marked = this.parentElement;
+        console.log(marked)
+        marked.style.textDecoration = 'line-through';
+      }else {
+        // on unchecked the text inside the parent element have no textDecoration 
+         console.log('unchecked');
+        let unmarked =this.parentElement;
+        unmarked.style.textDecoration = 'none';
+      }
+     
+      })
+   
+    
+    }
+  
+   
+
+     
